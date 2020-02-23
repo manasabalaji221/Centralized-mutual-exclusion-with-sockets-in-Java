47a6d220eb03a2219989eca6d34a4bd7854c8bd4
Manasa_Balaji <manasbalaji4857@gmail.com>
2020-02-22 22:47:18 -0600

    Add files via upload

---
+++ RawHttp.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Name: Manasa Balaji
+
+//NetId: mxb9247
+//Student ID: 1001689246
+
+
+
+package project3;
+
+/**
+ *
+ * @author manas
+ */
+public class RawHttp {
+    String s="";
+    public  void getValues(String seq)
+    {s=s+seq;
+          
+    }
+      public String getSeq()
+    {return s;          
+    }
+    
+}
